f1c1b48215e1b30f673e8c63aa72ac0a306b9e07
Ramesh Vyas <rameshovyas@gmail.com>
2022-12-15 17:54:52 +0530

    CU-333896e - Payoad encryption and code obfuscation to SDK - Added gulp-typescript so that whole process from transpiling to obfuscation is through gulp no tsc is used in build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 var obfuscate = require('gulp-obfuscate');
- 
+
 gulp.task('default', function () {
-    return gulp.src('src/app.js')
+    return gulp.src('src/app.ts')
     //.pipe(uglify())
+    .pipe(ts({
+        noImplicitAny: true        
+    }))
     .pipe(obfuscate())
     .pipe(gulp.dest('./dist/'));
 });
+
+   
