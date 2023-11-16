3d30ca1b61aec3c9c34528625076e7bccf80c118
joyson <joyson@logidots.com>
2023-11-16 16:19:35 +0530

    remove dev files and add some type classes for some functions

--- src/browser/src/core/index.ts
+++ src/browser/src/core/index.ts
@@ -1,13 +1,11 @@
 import * as platform from './platform';
 import * as location from './location';
 import * as dom from './dom';
-import * as http from './http';
 import * as event from './event';
 
 export {
     platform,
     location,
     dom,
-    http,
     event
 }
