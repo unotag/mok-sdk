ec65acabe1fc27ed8d05b9680e50cad26ca4d73b
joyson <joyson@logidots.com>
2023-12-14 12:38:18 +0530

    chore: minor optimizations on MokExpress middleware

--- src/browser/src/core/middleware.ts
+++ src/browser/src/core/middleware.ts
@@ -4,28 +4,32 @@ import { Express, Request, Response, NextFunction } from 'express';
 export const useMokExpress = (app: Express, mok: Promise<IMok>) => {
     app.set('trust proxy', true);
     app.use((req: Request, _res: Response, next: NextFunction) => {
-        mok.then(async (m) => {
-            m.visitorId = (m.setUserFn && await m.setUserFn()) || "undefined";
-            m.browserClient.setUser(m.visitorId);
-            let eventData = {
+        mok.then((m) => {
+            transmitUserActivity({
                 sdk: m.sdk + "+" + "express",
                 event_type: "visit",
                 visitor_id: m.visitorId,
                 domain: `${req.protocol}://${req.headers.host}`,
-                ip: req.ip,
+                ip: req.ip || "",
                 method: req.method,
                 currentPage: m.location.replaceTrailingSlash(`${req.protocol}://${req.headers.host}${req.path}`)
-            };
-
-            m.browserClient.addUserActivity("visit", eventData).then(_r => {
-                m.callback && m.callback(eventData);
-            }).catch((_e) => {
-                console.log("User activity could not be added");
-            });
-            next();
+            },m);
+            
         }).catch(_e => {
             console.log("Something went wrong while initializing the sdk");
-            next();
         });
+        next();
     });
+}
+
+export const transmitUserActivity = async (data: {[key:string]: string}, mok: IMok) => {
+    mok.visitorId = (mok.setUserFn && await mok.setUserFn()) || mok.visitorId || "undefined";
+    await mok.browserClient.setUser(mok.visitorId);
+
+    try {
+        await mok.browserClient.addUserActivity("visit", data);
+        mok.callback && mok.callback(data)
+    } catch(e) {
+        console.log("User activity could not be added");
+    }
 }
--- src/browser/src/interfaces/middleware.interface.ts
+++ src/browser/src/interfaces/middleware.interface.ts
@@ -3,4 +3,5 @@ import { Express } from 'express';
 
 export interface IMiddleware {
     useMokExpress(app: Express, mok: Promise<IMok>): void;
+    transmitUserActivity(data: {[key:string]: string}, mok: IMok): void;
 }
